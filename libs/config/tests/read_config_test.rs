088779ff6bc99e2c3d39bcc7bdefa5a351263f1f
derrick921213 <dlin12457@gmail.com>
2025-03-01 20:36:45 +0800

    fix: GRPC libs & config crate

--- libs/config/tests/read_config_test.rs
+++ libs/config/tests/read_config_test.rs
@@ -41,8 +41,7 @@ fn test_load_config_from_current_dir() {
     }
     let original_dir = env::current_dir().expect("Unable to get the current directory");
     env::set_current_dir(&dir).expect("Unable to set the current directory");
-    init_config_manager().expect("Unable to initialize the config manager");
-    let cmg = config::get_config_manager();
+    let cmg = get_config_manager(false);
     assert_eq!(cmg.get_trusted_domains().len(), 3);
     assert_eq!(cmg.get_allowed_origins().len(), 3);
     assert_eq!(cmg.get_allowed_methods().len(), 3);
