efa1ce8587c1e3f15bd39cc798a2cd5ad58b9f2a
derrick921213 <dlin12457@gmail.com>
2025-03-03 13:53:17 +0800

    fix: 測資內的get_config_manager

--- libs/config/tests/create_config_test.rs
+++ libs/config/tests/create_config_test.rs
@@ -2,7 +2,7 @@ use config::*;
 
 #[test]
 fn create_config() {
-    let _ = get_config_manager(true);
+    let _ = get_config_manager(Some(true));
     let path = expand_tilde("~/CHM/config/config.json");
     assert!(path.exists());
 }
--- libs/config/tests/read_config_test.rs
+++ libs/config/tests/read_config_test.rs
@@ -41,7 +41,7 @@ fn test_load_config_from_current_dir() {
     }
     let original_dir = env::current_dir().expect("Unable to get the current directory");
     env::set_current_dir(&dir).expect("Unable to set the current directory");
-    let cmg = get_config_manager(false);
+    let cmg = get_config_manager(None);
     assert_eq!(cmg.get_trusted_domains().len(), 3);
     assert_eq!(cmg.get_allowed_origins().len(), 3);
     assert_eq!(cmg.get_allowed_methods().len(), 3);
