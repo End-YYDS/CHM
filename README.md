bf6d72eeef270792f76d0fa9a99b9e45dac6bc23
derrick921213 <dlin12457@gmail.com>
2025-04-23 22:06:38 +0800

    Remove: 移除不屬於前端代碼

--- README.md
+++
@@ -1,4 +0,0 @@
-# CHM
-## 目錄結構
-- libs/*: 所有其他lib Crate存放處
-- ./*: 所有可執行的Crate
--- apps/ca/src/main.rs
+++
@@ -1,9 +0,0 @@
-use config::get_config_manager;
-use grpc::ca_server::start as start_ca_server;
-use grpc::common::Return;
-#[tokio::main]
-async fn main() -> Return<()> {
-    let cmg = get_config_manager(None);
-    start_ca_server(cmg).await?;
-    Ok(())
-}
