84c38a2415127790508a9eaefce4f6ec1f58859a
derrick921213 <dlin12457@gmail.com>
2025-05-22 17:20:09 +0800

    lefthook: fix

--- .lefthook/post-checkout/cleanup.cjs
+++ .lefthook/post-checkout/cleanup.cjs
@@ -2,7 +2,7 @@ const { execSync } = require("child_process");
 if (process.argv[4] === "1") {
   console.log("Cleaning up untracked & ignored files via git clean -fdX...");
   try {
-    execSync("git clean -fdX -e certs -e certs/**", { stdio: "inherit" });
+    execSync('git clean -fdx -e "certs/*"', { stdio: "inherit" });
     console.log("Cleanup completed.");
   } catch (err) {
     console.error("git clean 失敗", err.message);
